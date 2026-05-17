bfee091805a07b148a96a682dbcd12715589452a
muhammadt90900-ux <muhammadt90900@gmail.com>
2026-05-17 14:53:24 +0300

    Add notification permission request function

---
+++ frontend/js/notification.js
@@ -0,0 +1,9 @@
+// frontend/js/notification.js
+async function requestNotificationPermission() {
+  if ('Notification' in window) {
+    const permission = await Notification.requestPermission();
+    if (permission === 'granted') {
+      console.log('Notification permission granted.');
+    }
+  }
+}
